43a3d7d6e8edcf78b0a560e85277385a56b5e1b0
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2021-01-25 16:25:05 +0300

    feat: создать контроллер login для проверки email password и создания JWT

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,8 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUsers = (req, res) => {
   User.find({})
@@ -55,8 +58,27 @@ const createUser = (req, res) => {
     });
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' }
+      );
+
+      res.send({ token });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+}
+
 module.exports = {
   getUsers,
   getUserById,
-  createUser
+  createUser,
+  login
 };
