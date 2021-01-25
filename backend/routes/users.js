1de73df601ef94fa9bd1a1e551f41b4643fe207c
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2021-01-25 16:34:35 +0300

    feat: удалить роут создания и авторизации по-ля

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,9 @@
 const usersRouter = require('express').Router();
 
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const { getUsers, getUserById } = require('../controllers/users');
 
 usersRouter.get('/users', getUsers);
 
 usersRouter.get('/users/:id', getUserById);
-
-usersRouter.post('/users', createUser);
 
 module.exports = usersRouter;
